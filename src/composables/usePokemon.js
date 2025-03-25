9e4fe44dac4b188ca457b68f59541403eed08503
Ivan Parra <nesivapama@gmail.com>
2025-03-25 02:54:29 -0500

    refactored usePokemon

--- src/composables/usePokemon.js
+++ src/composables/usePokemon.js
@@ -4,19 +4,12 @@ import { usePokemonStore } from '../stores/pokemonStore'
 
 export function usePokemon() {
   const pokemonStore = usePokemonStore()
-  const { allPokemon, displayedPokemon, favorites, currentFilter } = storeToRefs(pokemonStore)
+  const { favorites, currentFilter } = storeToRefs(pokemonStore)
   const { toggleFavorite, updateDisplayedPokemon } = pokemonStore
 
   function toggleFavoriteAndFilter(pokemon) {
     toggleFavorite(pokemon)
     updateDisplayedPokemon()
-  }
-
-  function updateDisplayedPokemon() {
-    displayedPokemon.value =
-      currentFilter.value === 'favorites' && favorites.value.length
-        ? favorites.value
-        : allPokemon.value
   }
 
   function isFavorite(pokemon) {
